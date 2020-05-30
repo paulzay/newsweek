ece74855352333ee6a59b888f1d10fbd7d668f71
paulzay <paulotieno2@gmail.com>
2020-05-30 12:36:28 +0300

    implement datetime feature

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var dt = new Date();
+document.getElementById("datetime").innerHTML =
+  ("0" + (dt.getMonth() + 1)).slice(-2) +
+  " " +
+  ("0" + dt.getDate()).slice(-2) +
+  " " +
+  dt.getFullYear();
+document.getElementById("times").innerHTML =
+  ("0" + dt.getHours() + 1).slice(-2) +
+  ":" +
+  ("0" + dt.getMinutes() + 1).slice(-2);
